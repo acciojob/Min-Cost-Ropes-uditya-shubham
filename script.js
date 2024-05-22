6d8a26bbac5eaeac0b489dbf626a44a3e04e9f31
Uditya Kumar <shubhamkumaar2001@gmail.com>
2024-05-22 10:43:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+
+function myfunc(a,b){
+    return a-b;
+}
+let sum=0;
 function mincost(arr)
 { 
 //write your code here
 // return the min cost
+
+
+
+
+while(arr.length>1)
+    {
+        arr.sort(myfunc);
+
+        let a=arr.shift();
+        let b=arr.shift();
+        let c=a+b;
+        sum+=c;
+        arr.push(c);
+    }
+
+    // console.log(arr);
+
+
+
+    return sum;
+
   
 }
+
 
 module.exports=mincost;
